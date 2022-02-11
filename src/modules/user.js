38b590feaa282f1c14291882b5ccfbf6913a8ac4
Ruslan Matyushev <grandest.ua@gmail.com>
2022-02-11 10:47:27 +0200

    fix create first user

--- src/modules/user.js
+++ src/modules/user.js
@@ -1,28 +1,41 @@
 import mongoose from 'mongoose'
+
 const { model, Schema } = mongoose
 
 const User = model('User', new Schema({
-	unique_id: Number,
-	name: String,
-	surname: String,
-	patronymic: String,
-	login: String,
-	password: String,
-	role: {
-		type: String,
-		enum: ['admin', 'user', 'client']
-	},
-	email: String,
-	supervisor: {
-		type: Boolean,
-		default: false,
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-	},
+  unique_id: Number,
+  name: String,
+  surname: String,
+  patronymic: String,
+  login: String,
+  password: String,
+  role: {
+    type: String,
+    enum: ['admin', 'user', 'client']
+  },
+  email: String,
+  supervisor: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 }))
 
 export default User
 
-// db.user.insert({unique_id: 1, login: "admin", password: "password", role: "admin", supervisor: true})
+User.findOne()
+  .then(data => {
+    if (!data) {
+      User.create({
+        unique_id: 1,
+        login: 'admin',
+        password: 'password',
+        role: 'admin',
+        supervisor: true
+      })
+    }
+  })
+  .catch(e => console.error(e))
